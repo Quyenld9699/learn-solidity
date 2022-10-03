6d8d15ecd5e3d8c366c136773c4d26b874e37c0a
quyenld9699 <quyenld9699@gmail.com>
2022-10-03 09:43:41 +0700

    learn modifier and time

--- contracts/MyCoin.sol
+++ contracts/MyCoin.sol
@@ -5,23 +5,39 @@ contract MyCoin {
     address public minter;
     mapping (address => uint) public balance;
 
-    event sent(address from, address to, uint amount);
+    event Sent(address from, address to, uint amount);
+
+    modifier onlyMinter {
+        require(msg.sender == minter, "You are not host of coin");
+        _;
+    }
+    modifier checkamount(uint amount) {
+        require(amount < 1e60);
+        _;
+    }
+    modifier checkEnoughtMoney (uint amount) {
+        require(amount <= balance[msg.sender], "Khong du tien dau, nap them di!");
+        _;
+    }
+
 
     constructor() {
         minter = msg.sender;
     }
 
-    function mint(address reciver, uint amount) public {
-        require(msg.sender == minter, "You are not host of coin");
-        require(amount < 1e60);
+    function mint(address reciver, uint amount) public onlyMinter checkamount(amount){
+        // require(msg.sender == minter, "You are not host of coin");
+        // require(amount < 1e60);
 
         balance[reciver] += amount;
     }
 
-    function send(address reciver, uint amount) public {
-        require(amount <= balance[msg.sender], "Khong du tien dau, nap them di!");
+    function send(address reciver, uint amount) public checkEnoughtMoney(amount) {
+        // require(amount <= balance[msg.sender], "Khong du tien dau, nap them di!");
 
         balance[reciver] += amount;
         balance[msg.sender] -= amount;
+
+        emit Sent(msg.sender, reciver, amount);
     }
 }
--- contracts/People.sol
+++ contracts/People.sol
@@ -5,16 +5,31 @@ contract People {
     uint public countPerson = 0;
     mapping( address => Person ) public dataArr;
 
+    enum Level {Beginer, Inter, Advance}
+
     struct Person {
         address walletAddress;
         string fullName;
+        Level myLever;
         uint age;
         bool sex;
+        uint createTime;
     }
 
     function addPerson(string memory fullName, uint age, bool sex ) public {
-        dataArr[msg.sender]  =  Person(msg.sender,fullName, age, sex);
+        dataArr[msg.sender]  =  Person(msg.sender,fullName, Level.Beginer ,age, sex, block.timestamp);
         countPerson += 1;
+    }
+    
+    function getPlayerLevel(address addressPlayer) public view returns (Level) {
+        return dataArr[addressPlayer].myLever;
+    }
+
+    function changePlayerLevel(address personAddress) public {
+        Person storage person = dataArr[personAddress];
+        if (block.timestamp >= person.createTime + 15) {
+            person.myLever = Level.Inter;
+        }
     }
 
 }
